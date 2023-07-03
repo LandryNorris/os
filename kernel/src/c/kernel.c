054497a00154552033e5d7634e5de7c0bc61acd0
Landry <landry.norris@live.com>
2023-07-03 18:19:14 -0500

    cleanup various hacks and temporary solutions

--- kernel/src/c/kernel.c
+++ kernel/src/c/kernel.c
@@ -3,6 +3,6 @@
 
 void kernel_main(void) {
     terminal_initialize();
-    /* Newline support is left as an exercise. */
+
     puts("First Line\nSecond Line\nA very long line that will certainly overflow the 80 character limit of this text mode, causing it to wrap to the next line\nAnother Line");
 }
