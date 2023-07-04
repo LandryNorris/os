b05c3653236817e5e14e01346bdeba81218e23f9
Landry <landry.norris@live.com>
2023-07-04 08:38:36 -0500

    Add strcat method

--- kernel/src/c/kernel.c
+++ kernel/src/c/kernel.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <terminal.h>
+#include <string.h>
 
 void kernel_main(void) {
     terminal_initialize();
-    /* Newline support is left as an exercise. */
-    printf("First Line\nSecond Line\nA very long line that will certainly overflow the 80 character limit of this text mode, causing it to wrap to the next line\nAnother Line\n");
 
-    printf("size of int: %d\n", sizeof(int));
-    printf("size of long: %d\n", sizeof(long));
-    printf("size of long long: %d\n", sizeof(long long));
-    printf("size of double: %d\n", sizeof(double));
-    printf("Long number: %d\n", 100000000000);
-    printf("Testing numbers: %d %f %f %f %f\n", 100, 1.0, 0.0, 12345678.901234567890, -12345.6789);
+    char s1[100] = "Hello, ";
+    char* s2 = "World";
+
+    strcat(s1, s2);
+    puts(s1);
+    putchar('\n');
+
+    char first[100] = "Here ";
+    char* second = "is ";
+    char* third = "some ";
+    char* fourth = "text\n";
+
+    strcat(first, second);
+    strcat(first, third);
+    strcat(first, fourth);
+
+    puts(first);
 }
