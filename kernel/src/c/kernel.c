631b8de5a0fe8f048d2bd4c212a3fcb849053a67
Landry <landry.norris@live.com>
2023-07-04 07:56:18 -0500

    implement printf

--- kernel/src/c/kernel.c
+++ kernel/src/c/kernel.c
@@ -4,5 +4,12 @@
 void kernel_main(void) {
     terminal_initialize();
     /* Newline support is left as an exercise. */
-    puts("First Line\nSecond Line\nA very long line that will certainly overflow the 80 character limit of this text mode, causing it to wrap to the next line\nAnother Line");
+    printf("First Line\nSecond Line\nA very long line that will certainly overflow the 80 character limit of this text mode, causing it to wrap to the next line\nAnother Line\n");
+
+    printf("size of int: %d\n", sizeof(int));
+    printf("size of long: %d\n", sizeof(long));
+    printf("size of long long: %d\n", sizeof(long long));
+    printf("size of double: %d\n", sizeof(double));
+    printf("Long number: %d\n", 100000000000);
+    printf("Testing numbers: %d %f %f %f %f\n", 100, 1.0, 0.0, 12345678.901234567890, -12345.6789);
 }
--- libc/src/c/stdio/printf.c
+++ libc/src/c/stdio/printf.c
@@ -1,4 +1,126 @@
 
+#include <stdarg.h>
+#include <stdio.h>
+
+void printInt(long num) {
+    // Handle negative numbers
+    if (num < 0) {
+        putchar('-');
+        num = -num;
+    }
+
+    // Handle zero separately
+    if (num == 0) {
+        putchar('0');
+        return;
+    }
+
+    long integralPart = (long)num;
+    long divisor = 1;
+    while (divisor <= integralPart / 10) {
+        divisor *= 10;
+    }
+    while (divisor > 0) {
+        int digit = integralPart / divisor;
+        putchar(digit + '0');
+        integralPart %= divisor;
+        divisor /= 10;
+    }
+}
+
+void printDouble(double num, int minDigitsBeforeDecimal, int minDigitsAfterDecimal) {
+    // Handle negative numbers
+    if (num < 0) {
+        putchar('-');
+        num = -num;
+    }
+
+    // Handle zero separately
+    if (num == 0) {
+        putchar('0');
+        return;
+    }
+
+    // Calculate the number of digits before the decimal point
+    int numDigitsBeforeDecimal = 0;
+    long temp = (long)num;  // Typecast to integer for counting digits
+    while (temp > 0) {
+        temp /= 10;
+        numDigitsBeforeDecimal++;
+    }
+
+    long integral = (long) num;
+    if(integral < 0) integral *= -1;
+
+    // Print leading zeroes
+    if (numDigitsBeforeDecimal < minDigitsBeforeDecimal) {
+        int leadingZeros = minDigitsBeforeDecimal - numDigitsBeforeDecimal;
+        while (leadingZeros > 0) {
+            putchar('0');
+            leadingZeros--;
+        }
+    }
+
+    //print the digits before the decimal
+    printInt(integral);
+
+    putchar('.');
+
+    // Print digits after the decimal point
+    int numDigitsAfterDecimal = minDigitsAfterDecimal;
+    while (numDigitsAfterDecimal > 0) {
+        num *= 10;
+        char c = ((long)num % 10) + '0';
+        if(c < '0') c = '0'; //hack for bug due to overflow
+        putchar(c);
+        numDigitsAfterDecimal--;
+    }
+}
+
 int printf(const char* __restrict s, ...) {
+    va_list parameters;
+    va_start(parameters, s);
+
+    int isParsingFormat = 0;
+    int isEscaped = 0;
+    for(int i = 0; s[i] != '\0'; i++) {
+        char c = s[i];
+        if(!isParsingFormat && !isEscaped && c == '%') {
+            isParsingFormat = 1;
+            continue;
+        } else if(!isEscaped && c == '\\') {
+            isEscaped = 1;
+            continue;
+        }
+
+        if(isParsingFormat) {
+            if(c == '%') {
+                putchar('%');
+                isParsingFormat = 0;
+            } else if(c == 'd') {
+                int d = va_arg(parameters, int);
+                printInt(d);
+                isParsingFormat = 0;
+            } else if(c == 'f') {
+                double d = va_arg(parameters, double);
+                printDouble(d, 1, 6);
+                isParsingFormat = 0;
+            } else if(c == 's') {
+                char* string = va_arg(parameters, char*);
+                puts(string);
+                isParsingFormat = 0;
+            }
+        } else if(isEscaped) {
+            if(c == '\\') {
+                putchar('\\');
+            } else if(c == 'n') {
+                putchar('\n');
+            }
+            isEscaped = 0;
+        } else {
+            putchar(c);
+        }
+    }
+
     return 0;
 }
