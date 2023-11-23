cede3c22258c50fcb646ce252fd4060aa9470396
LandryNorris <landry.norris@live.com>
2023-11-22 21:44:13 -0600

    Hold list of child nodes in VfsNode

--- kernel/include/vfs.h
+++ kernel/include/vfs.h
@@ -10,7 +10,6 @@ struct VfsNode;
 typedef void (*openVfs) (struct VfsNode* node, uint32_t flags);
 typedef int (*readVfs) (struct VfsNode* node, uint8_t* buffer, uint32_t length);
 typedef int (*writeVfs) (struct VfsNode* node, uint8_t* data, uint32_t length);
-typedef struct VfsNode* (*listVfs) (struct VfsNode* node);
 typedef struct VfsNode* (*getVfs) (struct VfsNode* node, char* name);
 typedef void (*closeVfs) (struct VfsNode* node);
 
@@ -18,12 +17,13 @@ typedef struct {
     char name[256];
     uint8_t flags;
     void* device;
+    int numChildren;
+    struct VfsNode* children;
 
     openVfs open;
     readVfs read;
     writeVfs write;
     closeVfs close;
-    listVfs list;
     getVfs get;
 } VfsNode;
 
