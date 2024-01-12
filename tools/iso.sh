264d360b3c19a346a4e0f3828de1e3f3e4e8c8bf
LandryNorris <landry.norris@live.com>
2024-01-11 19:54:31 -0600

    fix qemu script

--- tools/iso.sh
+++ tools/iso.sh
@@ -1,8 +1,6 @@
-./build.sh
-
 cd ..
 
-if grub-file --is-x86-multiboot2 ./cmake-build-debug/kernel; then
+if grub-file --is-x86-multiboot2 $1; then
   echo multiboot confirmed
 else
   echo the file is not multiboot
@@ -12,7 +10,7 @@ fi
 mkdir -p ./build/iso/boot
 mkdir ./build/iso/boot/grub
 
-cp ./cmake-build-debug/kernel ./build/iso/boot/os.bin
+cp $1 ./build/iso/boot/os.bin
 cp ./grub/grub.cfg ./build/iso/boot/grub/grub.cfg
 
 grub-mkrescue -o build/os.iso ./build/iso
