bd76bd876c949a0557df3aae08b439240bffda46
LandryNorris <landry.norris@live.com>
2024-01-27 13:18:05 -0600

    Add script to run tests

---
+++ tools/runtests.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir ../build-tests
+cd ../build-tests
+
+cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ .. && cmake --build .
+
+./kernel_test --gtest_output=xml:report/report-kernel.xml
+./libc_test --gtest_output=xml:report/report-libc.xml
+./libk_test --gtest_output=xml:report/report-libk.xml
