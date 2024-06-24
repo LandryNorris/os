adcb45528bf39210362d25a43a5a943c20beca78
LandryNorris <landry.norris@live.com>
2024-06-23 19:13:50 -0500

    Parse information for timers

--- kernel/include/acpi/hpet.h
+++ kernel/include/acpi/hpet.h
@@ -4,6 +4,13 @@
 #include "acpiPreload.h"
 #include <stdbool.h>
 
+#define HPET_CONFIG_ENABLED_MASK 0x1U
+#define TIMER_CONFIG_SET_32_BIT_MODE_MASK (1U << 8U)
+#define TIMER_IS_PERIODIC_MASK (1U << 3U)
+#define TIMER_ENABLED_MASK (1U << 2U)
+#define TIMER_CONFIG_SUPPORTS_64_BIT_MASK (1U << 5U)
+#define TIMER_CONFIG_SUPPORTS_PERIODIC_MASK (1U << 4U)
+
 typedef struct {
     uint8_t revId;
     uint8_t numTimers;
@@ -12,15 +19,26 @@ typedef struct {
 } Capabilities;
 
 typedef struct {
+    bool enabled;
+    bool isPeriodic;
+    bool supports64Bit;
+    bool supportsPeriodic;
+    uint64_t* configurationRegister;
+    uint64_t* comparatorRegister;
+} Timer;
+
+typedef struct {
     uint32_t registerAddress;
     bool enabled;
     bool legacyReplacementEnabled;
     Capabilities capabilities;
 
-    uint8_t numComparators;
-
     uint16_t vendorId;
     uint32_t periodFemptoseconds;
+
+    uint64_t* counterRegister;
+
+    Timer timers[32];
 } HPET;
 
 int isHPETSignature(char* s);
--- kernel/src/c/acpi/hpet.c
+++ kernel/src/c/acpi/hpet.c
@@ -9,6 +9,18 @@ int isHPETSignature(char* s) {
     return s[0] == 'H' && s[1] == 'P' && s[2] == 'E' && s[3] == 'T';
 }
 
+uint64_t* timerConfigRegister(int timer) {
+    // NOLINTNEXTLINE
+    uint32_t offset = 0x100 + 0x20*timer;
+    return (uint64_t*) (hpet.registerAddress + offset);
+}
+
+uint64_t* timerComparatorRegister(int timer) {
+    // NOLINTNEXTLINE
+    uint32_t offset = 0x108 + 0x20*timer;
+    return (uint64_t*) (hpet.registerAddress + offset);
+}
+
 // NOLINTBEGIN(readability-magic-numbers)
 void parseHPETConfiguration() {
     uint64_t generalRegister = *(uint64_t*) hpet.registerAddress;
@@ -19,6 +31,22 @@ void parseHPETConfiguration() {
     hpet.vendorId = (generalRegister >> 16U) & 0xFFFF;
     hpet.periodFemptoseconds = generalRegister >> 32U;
 }
+
+void parseTimers() {
+    for(int i = 0; i < hpet.capabilities.numTimers; i++) {
+        Timer* timer = &hpet.timers[i];
+        timer->configurationRegister = timerConfigRegister(i);
+        timer->comparatorRegister = timerComparatorRegister(i);
+
+        uint64_t configValue = *timer->configurationRegister;
+
+        timer->enabled = (configValue & TIMER_ENABLED_MASK) != 0;
+        timer->isPeriodic = (configValue & TIMER_IS_PERIODIC_MASK) != 0;
+        timer->supports64Bit = (configValue & TIMER_CONFIG_SUPPORTS_64_BIT_MASK) != 0;
+        timer->supportsPeriodic = (configValue & TIMER_CONFIG_SUPPORTS_PERIODIC_MASK) != 0;
+    }
+}
+
 // NOLINTEND(readability-magic-numbers)
 
 void parseHPET(HPETLiteral* hpetTable) {
@@ -31,4 +59,51 @@ void parseHPET(HPETLiteral* hpetTable) {
     hpet.registerAddress = hpetTable->addressInfo.address;
 
     parseHPETConfiguration();
+    parseTimers();
+
+    hpet.counterRegister = (uint64_t*) (hpet.registerAddress + 0x20U);
+}
+
+void setHPETEnabled(bool isEnabled) {
+    hpet.enabled = isEnabled;
+    uint64_t* generalConfigRegister = (uint64_t*) (hpet.registerAddress + 0x10U);
+
+    if(isEnabled) {
+        *generalConfigRegister |= HPET_CONFIG_ENABLED_MASK;
+    } else {
+        *generalConfigRegister &= ~HPET_CONFIG_ENABLED_MASK;
+    }
+}
+
+uint64_t getMainCounterValue() {
+    // TODO(Landry): Check if upper 32 bits changes between reads to check
+    //  for overflow
+    return *hpet.counterRegister;
+}
+
+void setTimer64BitMode(int timer, bool is64BitEnabled) {
+    uint64_t* configRegister = timerConfigRegister(timer);
+
+    if(!is64BitEnabled) {
+        *configRegister |= TIMER_CONFIG_SET_32_BIT_MODE_MASK;
+    } else {
+        *configRegister &= ~TIMER_CONFIG_SET_32_BIT_MODE_MASK;
+    }
+}
+
+void setTimerComparator(int timer, uint64_t comparatorValue) {
+    *hpet.timers[timer].comparatorRegister = comparatorValue;
+}
+
+void startPeriodicTimer(int timer, int frequency) {
+    int periodFemptoSeconds = (int) (1e15 / frequency);
+
+    int numTicks = periodFemptoSeconds / hpet.periodFemptoseconds;
+
+
+
+    // The timer is still going. We want to minimize time between
+    // reading counter register and setting time to trigger.
+    int timeToTrigger = getMainCounterValue() + numTicks;
+    setTimerComparator(timer, timeToTrigger);
 }
